b831b395634a86cbb90e5f93e041153d276e0e13
Ghulam Muneer uddin <gmugsk@gmail.com>
2024-03-10 13:47:15 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,3 @@
+var personname = "Hello Eric, would you like to learn some Python today?";
+console.log(personname);
+export {};
---
+++ main.ts
@@ -0,0 +1,2 @@
+var personname : string = "Hello Eric, would you like to learn some Python today?"
+console.log(personname);
